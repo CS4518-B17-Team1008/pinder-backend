0f7f1364eb47b52d8891b575a2b5f0efa76e8d01
Can Alper <calper.ql@gmail.com>
2017-12-12 21:14:59 -0500

    Added echo

--- functions/index.js
+++ functions/index.js
@@ -5,8 +5,28 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const app = express();
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	  extended: true
+})); 
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
+
 app.get('/timestamp', (req, res) => {
+	console.log(`${Date.now()}`);
 	res.send(`${Date.now()}`);
 });
+
+app.post('/echo', (req, res) => {
+	console.log(req.body);
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write(req.body);
+	res.end();
+});
+
 
 exports.app = functions.https.onRequest(app);
